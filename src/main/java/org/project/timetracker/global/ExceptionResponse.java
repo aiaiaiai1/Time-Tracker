d6f6bc12efd28a9a27221a2aa52b3a3288cdbeb3
jihwan38 <sss203800@gmail.com>
2025-10-14 16:30:27 +0900

    feat: 정상적으로 작동하지 않던 ExceptionController 리팩토링

--- src/main/java/org/project/timetracker/global/ExceptionResponse.java
+++ src/main/java/org/project/timetracker/global/ExceptionResponse.java
@@ -1,9 +1,11 @@
 package org.project.timetracker.global;
 
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public class ExceptionResponse {
 
     private final Boolean success = false;
