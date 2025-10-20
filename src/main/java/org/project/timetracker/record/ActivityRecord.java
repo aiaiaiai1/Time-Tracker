dbe6a8fd48ed98a8d2d0804c5f57cd3cd88e701f
aiaiaiai1 <cvc100326@naver.com>
2025-10-20 20:43:53 +0900

    feat: 활동 기간의 총 시간 구하는 메서드 추가

--- src/main/java/org/project/timetracker/record/ActivityRecord.java
+++ src/main/java/org/project/timetracker/record/ActivityRecord.java
@@ -1,19 +1,14 @@
 package org.project.timetracker.record;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.project.timetracker.auth.User;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -60,6 +55,11 @@ public class ActivityRecord {
                 .category(category)
                 .memo(memoToSave)
                 .build();
+    }
+
+    public long getSpanMinutes() {
+        Duration span = Duration.between(startTime, endTime);
+        return span.toMinutes();
     }
 
 }
