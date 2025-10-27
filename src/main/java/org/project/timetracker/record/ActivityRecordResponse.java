20c848440fc98a1638496a0380a202f959ebcf2d
jihwan38 <sss203800@gmail.com>
2025-10-27 19:10:54 +0900

    feat: 서비스 레이어에서 mapper를 활용하도록 로직 변경

--- src/main/java/org/project/timetracker/record/ActivityRecordResponse.java
+++ src/main/java/org/project/timetracker/record/ActivityRecordResponse.java
@@ -2,15 +2,16 @@ package org.project.timetracker.record;
 
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import org.project.timetracker.record.data.ActivityRecordAllData;
 import org.project.timetracker.record.data.ActivityRecordMonthData;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ActivityRecordResponse(
         boolean success,
         String message,
-        ActivityRecordMonthData data
+        ActivityRecordAllData data
 ) {
-    public static ActivityRecordResponse success(String message, ActivityRecordMonthData data) {
+    public static ActivityRecordResponse success(String message, ActivityRecordAllData data) {
         return new ActivityRecordResponse(true, message, data);
     }
 }
