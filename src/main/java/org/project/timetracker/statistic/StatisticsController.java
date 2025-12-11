23a3687a97381bf83a81c9741223562410331939
aiaiaiai1 <cvc100326@naver.com>
2025-12-11 14:06:07 +0900

    refactor: 사용자의 목적을 카테고리가 아닌 입력했던 내용을 직접 보여주는 방식으로 수정

--- src/main/java/org/project/timetracker/statistic/StatisticsController.java
+++ src/main/java/org/project/timetracker/statistic/StatisticsController.java
@@ -179,12 +179,12 @@ public class StatisticsController {
                     .sum();
             List<CategoryData> targetCategoryData = getCategoryData(targetStatisticsByCategory, targetTotalMinutes);
             userTimeData.add(
-                    new UserTimeData(target.getUsername().substring(0, 2) + "***", (int) targetTotalMinutes, target.getClassifiedGoal(), targetCategoryData)
+                    new UserTimeData(target.getUsername().substring(0, 2) + "***", (int) targetTotalMinutes, target.getGoal(), targetCategoryData)
             );
         }
 
         userTimeData.sort(Comparator.comparing(UserTimeData::getTotalMinutes).reversed());
-        userTimeData.addFirst(new UserTimeData(user.getUsername(), (int) userTotalMinutes, user.getClassifiedGoal(), myData));
+        userTimeData.addFirst(new UserTimeData(user.getUsername(), (int) userTotalMinutes, user.getGoal(), myData));
 
         return ResponseEntity.ok(new ComparingResponse(true, userTimeData));
     }
